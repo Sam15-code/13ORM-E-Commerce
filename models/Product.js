e8317347c59d2a82d612f4ca43f06092f2e3920c
Sam15-code <ibtissam.dxb@gmail.com>
2023-03-16 23:31:56 -0500

    created category routes.js

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,42 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+   
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate:{
+      isDecimal: true,
+      
+    }
+    },
+
+    category_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references:{
+        model:'category',
+        key:'id'
+      }
+    
+    },
+    stock: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate:{
+        isNumeric: true,
+      }
+    },
   },
   {
     sequelize,
