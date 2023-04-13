903dd99bb5b8e6a39d8021e8b817df67e8ed2ee2
Sam15-code <ibtissam.dxb@gmail.com>
2023-04-12 20:50:23 -0500

    added routes. update, delete

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,21 +15,53 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoeyData = await Category.findByPk(req.params.id,{
+      include: [
+        { model: Product }
+      ],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
   // create a new category
+  try {
+    const categoeyData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async(req, res) => {
   // update a category by its `id` value
+  try {
+    const categoeyData = await Category.update(req.body,{where:{id:req.params.id}}
+    
+    );
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete a category by its `id` value
-});
 
+try {
+  const categoeyData = await Category.delete({where:{id:req.params.id}}
+  
+  );
+  res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
+})
 module.exports = router;
