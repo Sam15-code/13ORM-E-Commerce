6ac1b09566e7787bddfc35b9edb13ed368e32ece
Sam15-code <ibtissam.dxb@gmail.com>
2023-04-12 21:04:56 -0500

    added product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -109,8 +109,16 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async(req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.delete({where:{id:req.params.id}}
+    
+    );
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
